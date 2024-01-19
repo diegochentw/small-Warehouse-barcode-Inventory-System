0bc97ae108eff02558e5332d05ff1b0c082c94dc
diegochentw <shc.taiwan@gmail.com>
2024-01-19 16:39:41 +0800

    update gitignore

--- rma/db.py
+++ rma/db.py
@@ -6,13 +6,26 @@ from flask import current_app, g
 # g 也是一個全域變數，但它主要用於存儲應用程式上下文中的數據，以便在不同的函式之間共享數據，而不需要額外的參數傳遞。
 
 # 連結資料庫
+import sqlite3
+
 def get_db():
+    """
+    Get the SQLite database connection object.
+
+    If the 'db' variable does not exist in the global 'g' object, create a new SQLite3 database connection
+    object and store it in the 'g.db' variable. The location and other settings of the database are configured
+    using the Flask application's configuration ('current_app.config').
+
+    Returns:
+        The SQLite database connection object.
+
+    """
     if 'db' not in g:
-        g.db = sqlite3.connect( # 如果 'db' 變數不存在，則這一行建立一個 SQLite3 資料庫連接對象，並將它儲存在 g.db 變數中。資料庫的位置和其他設置是使用 Flask 應用程式的配置（current_app.config）來設定的。
+        g.db = sqlite3.connect(
             current_app.config['DATABASE'],
             detect_types=sqlite3.PARSE_DECLTYPES
-        ) 
-        g.db.row_factory = sqlite3.Row  #設置資料庫連接對象的 row_factory 屬性，使其返回的查詢結果行具有字典樣式的存取方式。
+        )
+        g.db.row_factory = sqlite3.Row
 
     return g.db
 
@@ -24,10 +37,17 @@ def close_db(e=None):
 
 # 新建資料表
 def init_db():
+    """
+    Initialize the database by executing the schema.sql file.
+    """
     db = get_db()
 
     with current_app.open_resource('schema.sql') as f:
-        db.executescript(f.read().decode('utf8'))
+        # Read the contents of the schema.sql file
+        schema_sql = f.read().decode('utf8')
+        
+        # Execute the SQL script
+        db.executescript(schema_sql)
 
 @click.command('init-db')
 def init_db_command():
@@ -37,7 +57,22 @@ def init_db_command():
 
 
 # 註冊application
-def init_app(app): ## 為了在Flask應用程序中註冊 close_db 和 init_db_command 函數，以便它們可以被應用程序正確使用
-    app.teardown_appcontext(close_db) #函數的目的通常是關閉與資料庫的連接或進行一些類似的清理工作，以確保在請求處理後不會留下未關閉的資源。這有助於避免資源洩漏和提高應用程式的穩定性。
-    app.cli.add_command(init_db_command) #是一個用於初始化資料庫的命令，當您需要在應用程式中執行一些初始化工作時，可以透過命令行界面來執行
+def init_app(app):
+    """
+    Registers the `close_db` and `init_db_command` functions in the Flask application.
+    
+    :param app: The Flask application object.
+    """
+    app.teardown_appcontext(close_db) 
+    """
+    Registers the `close_db` function to be called when the application context is torn down.
+    This function is typically used to close database connections or perform similar cleanup tasks
+    to ensure that no unclosed resources are left after request handling. 
+    This helps avoid resource leaks and improves the stability of the application.
+    """
+    app.cli.add_command(init_db_command)
+    """
+    Adds the `init_db_command` command to the Flask CLI.
+    This command can be used to initialize the database.
+    """
 
